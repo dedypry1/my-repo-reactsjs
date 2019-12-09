d63146379468e1426c5d5eed169d95028409e56c
dedypry1 <57791683+dedypry1@users.noreply.github.com>
2019-12-09 15:02:37 +0700

    Updates

---
+++ precache-manifest.c53f397edfdf65e3bbf046e5972bd9b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3911a51ad1d44e297e45cd5c0d2be2b",
+    "url": "/dedypry1/dedypry1.github.io.git/index.html"
+  },
+  {
+    "revision": "4d2a2042751935ac1718",
+    "url": "/dedypry1/dedypry1.github.io.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ae957d10dd218dbb0e43",
+    "url": "/dedypry1/dedypry1.github.io.git/static/js/2.76b57dd1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dedypry1/dedypry1.github.io.git/static/js/2.76b57dd1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d2a2042751935ac1718",
+    "url": "/dedypry1/dedypry1.github.io.git/static/js/main.841ec6fa.chunk.js"
+  },
+  {
+    "revision": "5740b08ba9f42abf9937",
+    "url": "/dedypry1/dedypry1.github.io.git/static/js/runtime-main.107ed89b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dedypry1/dedypry1.github.io.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
